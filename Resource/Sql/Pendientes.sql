34efb313414d1b8298931b99304dea7f7cf85fe1
johnny9052 <alexander9052@gmail.com>
2017-01-27 15:14:36 -0500

    SQL para estudiantes

--- Resource/Sql/Pendientes.sql
+++ Resource/Sql/Pendientes.sql
@@ -1,37 +1,37 @@
 CREATE TABLE IF NOT EXISTS tipo_documento (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NULL,
-  `descripcion` VARCHAR(2000) NULL,
-  PRIMARY KEY (`id`));
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NULL,
+  descripcion VARCHAR(2000) NULL,
+  PRIMARY KEY (id));
 
 
 CREATE TABLE IF NOT EXISTS departmento (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(50) NULL,
-  `descripcion` VARCHAR(2000) NULL,
-  PRIMARY KEY (`id`));
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NULL,
+  descripcion VARCHAR(2000) NULL,
+  PRIMARY KEY (id));
 
 
 CREATE TABLE IF NOT EXISTS municipio (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT(11) NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(50) NULL,
   descripcion VARCHAR(2000) NULL,
-  iddepartmento INT,
+  iddepartmento INT(11),
   PRIMARY KEY (id),
-  foreign key (iddepartmento) references departmento(id)
+  foreign key (iddepartmento) references departmento(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 create table estudiante(
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT(11) NOT NULL AUTO_INCREMENT,
   primer_nombre VARCHAR(50) NULL,
   segundo_nombre VARCHAR(50) NULL,
   primer_apellido VARCHAR(50) NULL,
   segundo_apellido VARCHAR(50) NULL,
-  id_tipo_documento int NOT NULL,
-  numero_documento varchar(15),
-  fecha_nacimiento date,
-  id_municipio int NOT NULL,
+  id_tipo_documento INT(11) NOT NULL,
+  numero_documento VARCHAR(15) NOT NULL,
+  fecha_nacimiento DATE NULL,
+  id_municipio INT(11) NOT NULL,
   primary key(id),
   foreign key(id_tipo_documento) references tipo_documento(id) ON DELETE SET NULL ON UPDATE CASCADE,
-  foreign key(id_municipio) references minicipio(id) ON DELETE SET NULL ON UPDATE CASCADE
+  foreign key(id_municipio) references municipio(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
